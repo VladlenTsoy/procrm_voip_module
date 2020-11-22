be24127461af3d748a26753e893691e6c8adfef7
vladlen <vladlengg@gmail.com>
2020-11-23 04:14:00 +0500

    update procrm_voip.js

--- assets/js/procrm_voip.js
+++ assets/js/procrm_voip.js
@@ -12,6 +12,9 @@ function dial({toNum}) {
         url: admin_url + '/procrm_voip/setting/dial',
         success: function (response) {
             console.log(response)
+            if (response?.error?.code === 1000) {
+                alert_float('danger', 'Неверный номер!')
+            }
         },
         error: function (e) {
             console.error(e)
@@ -19,9 +22,25 @@ function dial({toNum}) {
     })
 }
 
+
+function openModal() {
+    $(document).append(`
+        <div class="procrm_voip_modal">
+            <div class="procrm_voip_modal_card">
+                <h4>Выберите номер</h4>
+                <div class="menu">
+                    <div class="item"></div>
+                    <div class="item"></div>
+                </div>
+            </div>
+        </div>
+    `)
+}
+
 /** Слушать клик на href-tel и выполнять вызов **/
 $(document).on('click', 'a[href^="tel:"]', function (e) {
     e.preventDefault()
-    const tel = e.currentTarget.href.replace('tel:', '');
+    let tel = e.currentTarget.href.replace('tel:', '');
+    tel = tel.replace('+', '')
     dial({toNum: tel})
 })
